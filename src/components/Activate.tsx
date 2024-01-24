b11d3d3f38c82dfff2c44bad1103a0e9b919dede
Ramia-aloufi <ramia.aloufi@gmail.com>
2024-01-24 12:59:13 +0300

    update activate account

--- src/components/Activate.tsx
+++ src/components/Activate.tsx
@@ -1,18 +1,25 @@
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { activateUser } from '../Servies/user'
+import showToast from '../helper/toast'
+import { useDispatch } from 'react-redux'
 
 const Activate = () => {
   const { token } = useParams()
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const handleActivate = (token: string | undefined) => {
-    token && activateUser(token)
-    navigate('/')
+    if(token){
+      activateUser(token)
+      navigate('/')
+    }else{
+      showToast('error activated account',false, dispatch)
+    }
   }
 
   return (
     <div className="flex items-center justify-center h-full flex-wrap">
-      <button onClick={() => handleActivate(token)}>Activate Account</button>
+      <button className='btn2' onClick={() => handleActivate(token)}>Activate Account</button>
     </div>
   )
 }
